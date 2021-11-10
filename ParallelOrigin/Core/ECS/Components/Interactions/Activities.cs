b556e3edb1ed9ffe8e077d7d9ba72f859006e3b9
Lars <behethangames@gmail.com>
2021-11-10 18:31:07 +0100

    -- Some fixes

--- ParallelOrigin/Core/ECS/Components/Interactions/Activities.cs
+++ ParallelOrigin/Core/ECS/Components/Interactions/Activities.cs
@@ -1,11 +1,17 @@
+#if SERVER
 using DefaultEcs;
+#endif
 
 namespace ParallelOrigin.Core.ECS.Components.Interactions {
-    
+
+#if SERVER
+
     /// <summary>
     /// A component which lets an entity chop a resource entity ( tree ) down. 
     /// </summary>
     public struct Chop {
         public Entity target;
     }
+    
+#endif
 }
