ef76ca12fba7415f1b0aed0678812aa2c1f3e3ed
Lars <behethangames@gmail.com>
2021-11-22 21:40:08 +0100

    -- Added a Entity command with 6 places

--- ParallelOrigin/Core/Network/EntityCommands.cs
+++ ParallelOrigin/Core/Network/EntityCommands.cs
@@ -1,6 +1,18 @@
 using LiteNetLib.Utils;
 
 namespace ParallelOrigin.Core.Network {
+
+    /// <summary>
+    /// Possible entity op codes
+    /// </summary>
+    public static class EntityOpCode {
+
+        public const byte CREATE = 0;
+        public const byte ADD = 1;
+        public const byte SET = 2;
+        public const byte REMOVE = 3;
+        public const byte DELETE = 4;
+    }
     
     /// <summary>
     /// Represents an entity command which will be send to the client for either creating or deleting an entity. 
@@ -150,6 +162,41 @@ namespace ParallelOrigin.Core.Network {
             t3Component = reader.Get<T3>();
             t4Component = reader.Get<T4>();
             t5Component = reader.Get<T5>();
+        }
+    }
+    
+    /**
+     * An entity command with four of its direct components to save bandwith.
+     */
+    public struct EntityCommand<T1, T2, T3, T4, T5, T6> : INetSerializable where T1 : struct, INetSerializable where T2 : struct, INetSerializable where T3 : struct, INetSerializable where T4 : struct, INetSerializable where T5 : struct, INetSerializable where T6 : struct, INetSerializable {
+
+        public EntityCommand command;
+
+        public T1 t1Component;
+        public T2 t2Component;
+        public T3 t3Component;
+        public T4 t4Component;
+        public T5 t5Component;
+        public T6 t6Component;
+
+        public void Serialize(NetDataWriter writer) {
+            writer.Put(command);
+            writer.Put(t1Component);
+            writer.Put(t2Component);
+            writer.Put(t3Component);
+            writer.Put(t4Component);
+            writer.Put(t5Component);
+            writer.Put(t6Component);
+        }
+
+        public void Deserialize(NetDataReader reader) {
+            command = reader.Get<EntityCommand>();
+            t1Component = reader.Get<T1>();
+            t2Component = reader.Get<T2>();
+            t3Component = reader.Get<T3>();
+            t4Component = reader.Get<T4>();
+            t5Component = reader.Get<T5>();
+            t6Component = reader.Get<T6>();
         }
     }
 
