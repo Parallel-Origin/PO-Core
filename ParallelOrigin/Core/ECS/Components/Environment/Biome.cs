7e5695e3e2db151e6f243536325878b9e049e7fc
Lars <behethangames@gmail.com>
2022-07-28 18:18:02 +0200

    -- Added first theoretical mob spawning algorithm aswell as the mob prototyper

--- ParallelOrigin/Core/ECS/Components/Environment/Biome.cs
+++ ParallelOrigin/Core/ECS/Components/Environment/Biome.cs
@@ -1,3 +1,4 @@
+using System;
 using ParallelOrigin.Core.Base.Classes;
 using ParallelOrigin.Core.ECS.Components.Items;
 using ParallelOriginGameServer.Server.Extensions;
@@ -10,39 +11,53 @@ namespace ParallelOrigin.Core.ECS.Components.Environment {
     public enum NoiseCondition {
         GREATER, SMALLER
     }
-    
-    /// <summary>
-    /// Represents a entity for a biome layer with some conditions like weight and noise for being spawned in. 
-    /// </summary>
-    public partial struct BiomeEntity : IWeight{
 
-        public string type;
-        public float weight;
-
-        public float Weight => weight;
-    }
-    
     /// <summary>
-    /// A biome
+    /// Represents a biome. 
     /// </summary>
     public struct Biome : IWeight{
 
         public byte biomeCode;
         public float weight;
-        
+
         public float Weight { get => weight; set => weight = value; }
     }
 
     /// <summary>
     /// A biome which is dominated by european forests mostly
     /// </summary>
-    public struct Woodland{ public BiomeEntity[] spawnables; }
+    public struct Woodland {
+
+        public byte resolution;
+        public BiomeEntity[] spawnableResources;
+        public BiomeEntity[] spawmableMobs;
+    }
+
+    /// <summary>
+    /// A biome which is dominated by plains, grass and rocks mostly. Dry
+    /// </summary>
+    public struct Grassland {
+        
+        public byte resolution;
+        public BiomeEntity[] spawnableResources; 
+        public BiomeEntity[] spawmableMobs;
+    }
     
     /// <summary>
-    /// A biome which is dominated by plains and grass mostly
+    /// A component which defines sa forest layer.
+    /// Basically a noise 2D grid that should represent possible spawn-points for forests. 
     /// </summary>
-    public struct Grassland{ public BiomeEntity[] spawnables; }
+    public struct ForestLayer {
+        public NoiseGeocoordinates[,] noiseData;
+    }
     
+    /// <summary>
+    /// A component which defines a rock layer
+    /// Basically a noise 2D grid that should represent possible rock spawn points. 
+    /// </summary>
+    public struct RockLayer {
+        public NoiseGeocoordinates[,] noiseData;
+    }
     
     /// <summary>
     /// An struct which stores a noise value assigned to certain geo-coordinates
@@ -58,6 +73,21 @@ namespace ParallelOrigin.Core.ECS.Components.Environment {
             this.noise = noise;
         }
     }
+
+    ///////////////////////////
+    /// Biome entities and their attributes
+    //////////////////////////
+    
+    /// <summary>
+    /// Represents a entity for a biome layer like a tree or other environmental resource with some conditions like weight and noise for being spawned in. 
+    /// </summary>
+    public partial struct BiomeEntity : IWeight{
+
+        public string type;
+        public float weight;
+
+        public float Weight => weight;
+    }
     
     /// <summary>
     /// Partial biome entity which adds a forestNoise value to it because of the <see cref="ForestLayer"/>
@@ -65,16 +95,6 @@ namespace ParallelOrigin.Core.ECS.Components.Environment {
     public partial struct BiomeEntity {
         public NoiseCondition forestCondition;
         public float forestNoise;
-    }
-    
-    /// <summary>
-    /// A component which defines sa forest layer.
-    /// Basically a noise 2D grid that should represent possible spawn-points for forests. 
-    /// </summary>
-    public struct ForestLayer {
-        
-        public ushort resolution;
-        public NoiseGeocoordinates[,] noiseData;
     }
 
     /// <summary>
@@ -85,14 +105,18 @@ namespace ParallelOrigin.Core.ECS.Components.Environment {
         public float rockNoise;
     }
 
-    
     /// <summary>
-    /// A component which defines a rock layer
-    /// Basically a noise 2D grid that should represent possible rock spawn points. 
+    /// Addition for mob entities which require additional properties like grouping options or day
     /// </summary>
-    public struct RockLayer {
+    public partial struct BiomeEntity {
 
-        public ushort resolution;
-        public NoiseGeocoordinates[,] noiseData;
+        // If the entity is allowed to spawn in packs ( groups ), mostly for mob spawning
+        public bool pack;
+        public byte packSizeMin;
+        public byte packSizeMax;
+
+        // The times when this entity is allowed to spawn, always timespans
+        public ValueTuple<TimeSpan, TimeSpan>[] times;
     }
+
 }
