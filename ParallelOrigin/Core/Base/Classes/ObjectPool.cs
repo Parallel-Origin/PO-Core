8f53fa241620e3725a5461331f5dcdf23be51bf9
Lars <behethangames@gmail.com>
2022-08-04 21:30:31 +0200

    -- Added object pool and movement and animation for mobs

---
+++ ParallelOrigin/Core/Base/Classes/ObjectPool.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Concurrent;
+using System.Runtime.CompilerServices;
+
+namespace ParallelOrigin.Core.Base.Classes; 
+
+/// <summary>
+/// A pool of objects which can be used to pool items for short lived operations. 
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public static partial class ObjectPool<T> {
+
+    private static readonly ConcurrentBag<T> _objects = new();
+    
+    /// <summary>
+    /// Gets an object from the pool or creates a new one
+    /// </summary>
+    /// <returns></returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static T Get() {
+        
+        if(_objects.TryTake(out var item)) return item;
+
+        var newInstance = default(T);
+        return newInstance;
+    }
+    
+    /// <summary>
+    /// Returns an object to the pool which results in being added 
+    /// </summary>
+    /// <param name="item"></param>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static void Return(T item) => _objects.Add(item);
+}
---
+++ ParallelOrigin/Core/ECS/Components/AI/AI.cs
@@ -0,0 +1,11 @@
+using FluentBehaviourTree;
+
+namespace ParallelOrigin.Core.ECS.Components.AI; 
+
+/// <summary>
+/// A component which acts as an AI controller to controll the attached entity automaticly 
+/// </summary>
+public struct AIController {
+    
+    public IBehaviourTreeNode behaviourTree;
+}
