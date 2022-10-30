7a43537e1fe98200b8b05a827b5d16302dd5e05a
Lars <behethangames@gmail.com>
2022-10-30 14:10:57 +0100

    Splitted the anim command into two specialised cases and gave them only a entity reference since the entity always need to exist at that point

--- ParallelOrigin/Core/Network/AnimationCommands.cs
+++ ParallelOrigin/Core/Network/AnimationCommands.cs
@@ -3,64 +3,44 @@ using ParallelOrigin.Core.Extensions;
 
 namespace ParallelOrigin.Core.Network;
 
-/// <summary>
-///     Represents a bool params for animation controller
-/// </summary>
-public struct BoolParams : INetSerializable
-{
-    public string boolName;
-    public bool activated;
+    /// <summary>
+    /// Represents a bool params for animation controller
+    /// </summary>
+    public struct AnimationParamCommand : INetSerializable {
 
-    public void Serialize(NetDataWriter writer)
-    {
-        writer.PutFixedString(boolName, (ushort)boolName.Length);
-        writer.Put(activated);
+        public EntityReference entityReference;
+        public string boolName;
+        public bool activated;
+
+        public void Serialize(NetDataWriter writer) {
+            writer.Put(entityReference);
+            writer.PutFixedString(boolName, (ushort)boolName.Length);
+            writer.Put(activated);
+        }
+
+        public void Deserialize(NetDataReader reader) {
+            entityReference.Deserialize(reader);
+            boolName = reader.GetFixedString();
+            activated = reader.GetBool();
+        }
     }
-
-    public void Deserialize(NetDataReader reader)
-    {
-        boolName = reader.GetFixedString();
-        activated = reader.GetBool();
-    }
-}
-
-/// <summary>
-///     Represents a animation trigger.
-/// </summary>
-public struct Trigger : INetSerializable
-{
-    public string triggerName;
-
-    public void Serialize(NetDataWriter writer)
-    {
-        writer.PutFixedString(triggerName, (ushort)triggerName.Length);
-    }
-
-    public void Deserialize(NetDataReader reader)
-    {
-        triggerName = reader.GetFixedString();
-    }
-}
 
     /// <summary>
-    /// A simple struct representing a animation command representing a list of changed animation states.
-    /// Just because we cant use a alias to hide this generic shit. 
+    /// Represents a animation trigger. 
     /// </summary>
-    public struct AnimationCommand : INetSerializable {
+    public struct AnimationTriggerCommand : INetSerializable {
 
-        public EntityCommand command;
-        public Statefull<BoolParams> changedBoolParams;
-        public Trigger triggers;
+        public EntityReference entityReference;
+        public string triggerName;
 
-    public void Serialize(NetDataWriter writer)
-    {
-        writer.Put(changedBoolParams);
-        writer.Put(triggers);
-    }
+        public void Serialize(NetDataWriter writer) {
+            writer.Put(entityReference);
+            writer.PutFixedString(triggerName, (ushort)triggerName.Length);
+        }
 
-    public void Deserialize(NetDataReader reader)
-    {
-        changedBoolParams.Deserialize(reader);
-        triggers.Deserialize(reader);
+        public void Deserialize(NetDataReader reader) {
+            entityReference.Deserialize(reader);
+            triggerName = reader.GetFixedString();
+        }
     }
 }
