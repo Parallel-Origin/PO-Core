6880c1b1a617e16436df753b93ebfbc7c31a1f29
Lars <behethangames@gmail.com>
2023-05-11 02:48:06 +0200

    Some fixes and updated arch and arch eventbus.

--- ParallelOrigin/Core/ECS/Events/LifecycleEvents.cs
+++ ParallelOrigin/Core/ECS/Events/LifecycleEvents.cs
@@ -1,9 +1,14 @@
 #if SERVER
 using Arch.Core;
+using ParallelOrigin.Core.Base.Classes;
 using ParallelOrigin.Core.ECS.Components;
 
 namespace ParallelOrigin.Core.ECS.Events;
 
+/// <summary>
+///     The <see cref="CreateEvent"/> struct
+///     represents a <see cref="Entity"/> creation event.
+/// </summary>
 public ref struct CreateEvent
 {
     public Entity Entity;
@@ -16,6 +21,10 @@ public ref struct CreateEvent
     }
 }
 
+/// <summary>
+///     The <see cref="DestroyEvent"/> struct
+///     represents a <see cref="Entity"/> destruction event.
+/// </summary>
 public ref struct DestroyEvent
 {
     public Entity Entity;
@@ -25,6 +34,38 @@ public ref struct DestroyEvent
     {
         Entity = entity;
         Identity = ref identity;
+    }
+}
+
+/// <summary>
+///     The <see cref="ChunkCreatedEvent"/> struct
+///     represents an event for a newly created chunk <see cref="Entity"/>. 
+/// </summary>
+public ref struct ChunkCreatedEvent
+{
+    public Entity Entity;
+    public Grid Grid;
+
+    public ChunkCreatedEvent(Entity entity, Grid grid)
+    {
+        Entity = entity;
+        Grid = grid;
+    }
+}
+
+/// <summary>
+///     The <see cref="ChunkDestroyedEvent"/> struct
+///     represents an event for a destroyed chunk <see cref="Entity"/>. 
+/// </summary>
+public ref struct ChunkDestroyedEvent
+{
+    public Entity Entity;
+    public Grid Grid;
+
+    public ChunkDestroyedEvent(Entity entity, Grid grid)
+    {
+        Entity = entity;
+        Grid = grid;
     }
 }
 
